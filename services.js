65af8c8bb340dc1c1594ba79307a89725709783b
lorenaa-rs <lorerios.2000@gmail.com>
2021-07-05 21:12:59 -0500

    first commit

---
+++ services.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+var body_parser = require('body-parser');
+
+
+app.use(body_parser.urlencoded({extended:true}));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+var formulario = '<form method="post" action="/nacimiento">'
+    + '<label for="edad">¿Qué edad tienes?</label>'
+    + '<input type="text" name="edad" id="edad">'    
+    + '<input type="submit" value="Enviar"/>'
+    + '</form>';
+ 
+var cabecera = '<h1>Naciste el año</h1>';
+ 
+app.get('/nacimiento', function (req, res) {
+ 
+    res.send('<html><body>'
+            + cabecera
+            + formulario
+            + '</html></body>'
+    );
+ 
+});
+
+app.post('/nacimiento', function (req, res) {
+ 
+    var edad = req.body.edad || '';
+    var nacimiento = '';
+ 
+    if (edad != '')
+        nacimiento = 2015 - edad;
+ 
+    res.send('<html><body>'
+            + cabecera
+            + '<p>' + nacimiento + '</p>'
+            + formulario
+            + '</html></body>'
+    );
+ 
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
